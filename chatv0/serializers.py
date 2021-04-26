725e5a58630abd12e756c6a99dcb4496561d24e5
TranQuoc Tuan <68458412+tuantranquoc@users.noreply.github.com>
2021-04-26 10:34:06 +0700

    s

--- chatv0/serializers.py
+++ chatv0/serializers.py
@@ -5,16 +5,26 @@ from chatv0.models import Message
 
 
 class RoomSerializer(serializers.ModelSerializer):
-    user = ChatProfileSerializer(read_only=True, many=True)
     lasted_message = serializers.SerializerMethodField(read_only=True)
+    user_list = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Room
-        fields = ['id','user','lasted_message']
-
+        fields = ['id','lasted_message','user_list']
     @staticmethod
     def get_lasted_message(obj):
         lasted_message = Message.objects.filter(room__id=obj.id).order_by("-created_at").first()
         if lasted_message:
             return lasted_message.content
         return None
-        
+
+    def get_user_list(self, obj):
+        request = self.context.get("request")
+        if request.user:
+            user_list = obj.user.all()
+            if user_list:
+                u_l = []
+                for u in user_list:
+                    if u != request.user:
+                        u_l.append({"username":u.username,"avatar": u.profile.avatar.url or None})
+                return u_l
+        return None
