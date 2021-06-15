2ffad0b18f38b98995c214e33fbc1f29e850b370
TranQuoc Tuan <68458412+tuantranquoc@users.noreply.github.com>
2021-06-15 14:02:26 +0700

    update chat create

--- chatv0/controller/urls.py
+++ chatv0/controller/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from chatv0.controller import rest as chat_api
 
 urlpatterns = [
-    path('', chat_api.create_chat_room),
+    path('create/', chat_api.create_chat_room),
     path('find/', chat_api.find_room),
     path('all/', chat_api.get_all_room),
     # path('get/room', chat_api.get_room_by_user),
