1133d464249812913617917de010198a003481fc
TranQuoc Tuan <68458412+tuantranquoc@users.noreply.github.com>
2021-06-30 18:35:18 +0700

    update notify serializer

--- notify/serializers.py
+++ notify/serializers.py
@@ -1,6 +1,8 @@
 from .models import SignalRoom, Notification, UserNotify
 from rest_framework import serializers
 from notify.models import NotificationChange
+from post.serializers import CommunitySerializer
+from community.models import Community
 
 
 class SignalRoomSerializer(serializers.ModelSerializer):
@@ -19,11 +21,13 @@ class UserNotifySerializers(serializers.ModelSerializer):
     user = serializers.SerializerMethodField(read_only=True)
     type = serializers.SerializerMethodField(read_only=True)
     entity_id = serializers.SerializerMethodField(read_only=True)
+    message =  serializers.SerializerMethodField(read_only=True)
+    community = serializers.SerializerMethodField(read_only=True)
     # title = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = UserNotify
-        fields = ['id', 'message', 'status','user', 'type','entity_id']
+        fields = ['id', 'message', 'status','user', 'type','entity_id','community']
 
     @staticmethod
     def get_user(obj):
@@ -41,7 +45,7 @@ class UserNotifySerializers(serializers.ModelSerializer):
             if x.entity_type.id == 4:
                 return "new_comment"
             if x.entity_type.id == 6:
-                title = x.post.title
+                title = "new_post"
                 return title
         return None
 
@@ -51,6 +55,27 @@ class UserNotifySerializers(serializers.ModelSerializer):
             if x.post:
                 return x.post.id
         return None
- 
+    
+    @staticmethod
+    def get_message(obj):
+        for x in obj.notification_object.all():
+            if x.entity_type.id == 6:
+                title = x.post.title
+                return title
+        return None
+    
+    @staticmethod
+    def get_community(obj):
+        for x in obj.notification_object.all():
+            if x.entity_type.id == 6:
+                community = x.post.community
+                print(community)
+                community = Community.objects.filter(community_type=community).first()
+                if community:
+                    if community.avatar:
+                        return {"community_type":community.community_type,"avatar":community.avatar.url}
+                return {"community_type":community.community_type,"avatar":None}
+
+            return None
  
 
